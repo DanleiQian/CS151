a1a3d6056a259d012e3347b5a4a51b9ea59f4009
Danlei Qian <danleiqian@Danleis-MBP.T-mobile.com>
2020-09-08 16:34:21 -0700

    First Commit

---
+++ src/HW1.java
@@ -0,0 +1,7 @@
+public class HW1 {
+    public static void main(String[] args) {
+        System.out.println("      _      _      _     --------------");
+        System.out.println("   __(.)< __(.)> __(.)= < Hello, world!|");
+        System.out.println("   [___)  [___)  [___)    --------------");
+    }
+}
